04e49f7d27d5a77990a8ada587f31c74ef1b8e9a
Александр <palenko.as@mail.ru>
2021-10-12 21:24:40 +0300

    3. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -1,9 +1,6 @@
 package ru.job4j.collection;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static java.util.Objects.hash;
 
@@ -13,22 +10,28 @@ public class User {
     
     private String name;
     private int children;
-    private static Calendar birthday = new GregorianCalendar();
+    private static Calendar birthday = new GregorianCalendar(1975, Calendar.OCTOBER, 6);
+    private static Date date = birthday.getTime();
 
-    public User(String name, int children, Calendar birthday) {
+    public User(String name, int children, Date date) {
         this.name = name;
         this.children = children;
-        User.birthday = birthday;
+        User.date = date;
     }
     public static void main(String[] args) {
-        birthday.set(1994, Calendar.OCTOBER, 13);
-        User user1 = new User("Igor", 1, birthday);
-        User user2 = new User("Nikolay", 0, birthday);
+        User user1 = new User("Igor", 1, date);
+        User user2 = new User("Nikolay", 0, date);
         map.put(user1, new Object());
         map.put(user2, new Object());
         for (Map.Entry<User, Object> maps: map.entrySet()) {
             User data = maps.getKey();
-            System.out.println(hash(data.hashCode()));
+            System.out.println(maps);
+            System.out.println(data.hashCode());
         }
-    }                                                                                   
+    }
+    
+    @Override
+    public int hashCode() {
+        return hash(name, children);
+    }
 }
