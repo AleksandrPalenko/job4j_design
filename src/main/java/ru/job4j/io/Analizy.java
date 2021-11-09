f973cc2c540d8fb1659255cd8df772bcb749053d
Александр <palenko.as@mail.ru>
2021-11-09 19:06:40 +0300

    2. Анализ доступности сервера. [#859 #225525]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Analizy {
+    public static void unavailable(String source, String target) {
+        List<String[]> rsl = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+           rsl = reader.lines().map(s -> s.split(" ")).collect(Collectors.toList());
+        } catch (Exception a) {
+            a.printStackTrace();
+        }
+        try (PrintWriter writer = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(target)
+                ))) {
+            for (String[] line: rsl) {
+                if (line[0].equals("400") || line[0].equals("500")) {
+                    writer.println("server is down " + line[1]);
+                } else if (line[0].equals("200")) {
+                    writer.println("server is up " + line[1]);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        unavailable("server.csv", "target.csv");
+    }
+}
