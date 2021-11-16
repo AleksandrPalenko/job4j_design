08958e4b9923d1830db02da7e90343b4567fa1e8
Александр <palenko.as@mail.ru>
2021-11-16 13:52:19 +0300

    2. Анализ доступности сервера. [#859 #225525]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -6,25 +6,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Analizy {
-    public static void unavailable(String source, String target) {
+    public void unavailable(String source, String target) {
         List<String[]> rsl = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
-           rsl = reader.lines().map(s -> s.split(" ")).collect(Collectors.toList());
-        } catch (Exception a) {
-            a.printStackTrace();
-        }
-        try (PrintWriter writer = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)
-                ))) {
+            PrintWriter writer = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)));
             boolean status = false;
+            rsl = reader.lines().map(s -> s.split(" ")).collect(Collectors.toList());
             for (String[] line: rsl) {
-                if (!status && (line[0].equals("400") || line[0].equals("500"))) {
-                    status = true;
-                    writer.print(line[1] + ";");
-                } else if (status && (line[0].equals("200") || line[0].equals("300"))) {
-                    status = false;
-                    writer.println(line[1]);
+                String lines = reader.readLine();
+                while (lines != null) {
+                    if (!status && (line[0].equals("400") || line[0].equals("500"))) {
+                        status = true;
+                        writer.print(line[1] + ";");
+                    } else if (status && (line[0].equals("200") || line[0].equals("300"))) {
+                        status = false;
+                        writer.println(line[1] + ";");
+                    }
                 }
             }
         } catch (Exception e) {
@@ -34,6 +31,7 @@ public class Analizy {
 
 
     public static void main(String[] args) {
-        unavailable("server.csv", "target.csv");
+        Analizy analizy = new Analizy();
+        analizy.unavailable("server.csv", "target.csv");
     }
 }
