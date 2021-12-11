c7d420a05454b113be4b3ca5753a66512b0ce865
Александр <palenko.as@mail.ru>
2021-12-12 00:45:44 +0300

    0. Что такое база данных. [#6860 #238884]

--- script.sql
+++ script.sql
@@ -1,11 +1,12 @@
-create table Car (
+create table car (
 	id serial primary key,
 	type varchar(255),
 	carName varchar(30),
 	price numeric
 );
-select * from Cars
+select * from Car
 insert into Car(type, carName, price) values('Sedan','BMW','2350000');
 update Car set carName = 'Lada', price = '1350000'
 insert into Car(type,carName,price) values('jeep','landCruzer','3500000')
 delete from Car
+update Car set carName = 'BMW' where id = '3'
