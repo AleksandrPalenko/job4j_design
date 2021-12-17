17d0ddd2d1a103678205096abe4f2082deaab4d3
Александр <palenko.as@mail.ru>
2021-12-17 14:32:58 +0300

    1. Схема прав пользователи и роли. [#1093 #241043]

--- job4j_postgre/create.sql
+++ job4j_postgre/create.sql
@@ -1,45 +1,51 @@
 create table users(
 	id serial primary key,
-	name text
+	name text,
+	role_id int reference role(id)
 );
 
 create table role(
 	id serial primary key,
-	nameOfRole varchar(255),
-	users_id int references users(id)
+	nameOfRole varchar(255)
 );
+
 create table rules(
 	id serial primary key,
-	nameOfRule varchar(255),
-	role_id int references role(id)
+	nameOfRule varchar(255)
 );
+
 create table role_rules(
 	id serial primary key,
 	role_id int references role(id),
 	rules_id int references rules(id)
 );
+
 create table item (
 	id serial primary key,
 	nameOfItem varchar(255),
-	users_id int references users(id)
+	users_id int references users(id),
+	category_id reference category(id),
+	state_id reference category(id)
 );
+
 create table comments (
 	id serial primary key,
 	name varchar(255),
 	item_id int references item(id)
 );
+
 create table attachs (
 	id serial primary key,
 	name varchar(255),
 	item_id int references item(id)
 );
+
 create table category (
 	id serial primary key,
-	nameOfCategory varchar(255),
-	item_id int references item(id)
+	nameOfCategory varchar(255)
 );
+
 create table state (
 	id serial primary key,
-	name varchar(255),
-	item_id int references item(id)
+	name varchar(255)
 );
