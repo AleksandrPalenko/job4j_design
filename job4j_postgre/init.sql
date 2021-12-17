256853ecb492e713a47094a17c5adbcfdee4b375
Александр <palenko.as@mail.ru>
2021-12-17 12:41:02 +0300

    1. Схема прав пользователи и роли. [#1093 #241043]

---
+++ job4j_postgre/init.sql
@@ -0,0 +1,13 @@
+insert into users(name) values('Ivan');
+insert into users(name) values('Aleksandr');
+insert into role(nameOfRole) values('programmer');
+insert into role(nameOfRole) values('lead');
+insert into rules(nameOfRule) values('commit');
+insert into rules(nameOfRule) values('task');
+select * from rules;
+insert into role_rules(role_id, rules_id) values(9, 9); 
+insert into role_rules(role_id, rules_id) values(10, 10);
+insert into comments(name) values ('check');
+insert into attachs(name) values ('git');
+insert into category(nameOfCategory) values('back-end');
+insert into state(name) values('done');
