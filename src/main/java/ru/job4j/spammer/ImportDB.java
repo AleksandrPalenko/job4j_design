542d9c60c872bf3c031f48611b22e20c5ccf1b36
Александр <palenko.as@mail.ru>
2021-12-26 11:48:35 +0300

    0.2. PrepareStatement [#379307 #242974]

--- src/main/java/ru/job4j/spammer/ImportDB.java
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -24,7 +24,7 @@ public class ImportDB {
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
             rd.lines().forEach(line -> {
                 String[] str = line.split(";");
-                if (str.length != 2 || !str[0].isEmpty() || !str[1].isEmpty()) {
+                if (str.length != 2 || str[0].isEmpty() || str[1].isEmpty()) {
                     throw new IllegalArgumentException("Invalid agruments");
                 } else {
                     users.add(new User(str[0], str[1]));
