940e9e753110c25f4911227490d7d9aa20551abc
Александр <palenko.as@mail.ru>
2022-01-14 18:52:01 +0300

    7. Scanner [#504791 #238690]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -7,17 +7,18 @@ import java.util.*;
 
 public class CSVReader {
 
-    public static void valid(String[] args) {
+    private void valid(String[] args) {
         if (args.length != 4) {
             throw new IllegalArgumentException("Invalid parameters");
         }
         ArgsName argsName = ArgsName.of(args);
+        File file = new File(argsName.get("path"));
+        if (!file.isDirectory() || !file.exists()) {
+            throw new IllegalArgumentException("File does not exists");
+        }
         for (String str : args) {
             argsName.get(str);
-            File file = new File(argsName.get("path"));
-            if (!file.isDirectory() || !file.exists()) {
-                throw new IllegalArgumentException("File does not exists");
-            } else if (str == null) {
+            if (str == null) {
                 throw new IllegalArgumentException(
                         String.format("The parameter %s is not found", str));
             }
@@ -28,29 +29,38 @@ public class CSVReader {
  */
 
     public static void handle(ArgsName argsName) throws Exception {
-        List<String> filterVal = new ArrayList<>();
+        List<Integer> filterVal = new ArrayList<>();
         List<String> list = new ArrayList<>();
         String delimiter = argsName.get("delimiter");
         String out = argsName.get("out");
         String path = argsName.get("path");
         String filter = argsName.get("filter");
-        var scanner = new Scanner(new BufferedReader(new FileReader(argsName.get(path))));
-        if (scanner.hasNext()) {
-            String[] arg = scanner.nextLine().split(delimiter);
-            StringJoiner tempLine = new StringJoiner(";");
-            for (int i = 0; i < arg.length; i++) {
-                if (filter.contains(arg[i])) {
-                    filterVal.add(String.valueOf(i));
-                } else if (filterVal.contains(arg[i])) {
-                    tempLine.add(String.valueOf(i));
+        try (var scanner = new Scanner(new File(path))) {
+            if (scanner.hasNext()) {
+                String[] arg = scanner.nextLine().split(delimiter);
+                for (int i = 0; i < arg.length; i++) {
+                    if (filter.contains(arg[i])) {
+                        filterVal.add(i);
+                    }
                 }
             }
-            list.add(tempLine.toString());
         }
-        if ("stdout".equals(out)) {
-            list.forEach(System.out::println);
-        } else {
-            saveResult(list, out);
+        try (var scanner = new Scanner(new File(path))) {
+            while (scanner.hasNext()) {
+                String[] arg = scanner.nextLine().split(delimiter);
+                StringJoiner tempLine = new StringJoiner(";");
+                for (int i = 0; i < arg.length; i++) {
+                    if (filterVal.contains(i)) {
+                        tempLine.add(arg[i]);
+                    }
+                }
+                list.add(tempLine.toString());
+            }
+            if ("stdout".equals(out)) {
+                list.forEach(System.out::println);
+            } else {
+                saveResult(list, out);
+            }
         }
     }
 
@@ -65,7 +75,8 @@ public class CSVReader {
     }
 
     public static void main(String[] args) throws Exception {
-        valid(args);
+        CSVReader csvReader = new CSVReader();
+        csvReader.valid(args);
         ArgsName argsName = ArgsName.of(args);
         handle(argsName);
     }
