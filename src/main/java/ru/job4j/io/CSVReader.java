9eb997adaa9704d35ad9c435245bf01d4059db4b
Александр <palenko.as@mail.ru>
2022-01-14 13:42:29 +0300

    7. Scanner [#504791 #238690]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -3,10 +3,7 @@ package ru.job4j.io;
 import ru.job4j.serialization.json.A;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class CSVReader {
 
@@ -14,7 +11,6 @@ public class CSVReader {
         if (args.length != 4) {
             throw new IllegalArgumentException("Invalid parameters");
         }
-        ArgsName.of(args);
         if (args[0].startsWith("-path")) {
             File file = new File(args[1]);
             if (!file.isDirectory() || !file.exists()) {
@@ -22,9 +18,50 @@ public class CSVReader {
             }
         }
     }
-
+/*
+-path=file.csv -delimiter=";"  -out=stdout -filter=name,age
+ */
 
     public static void handle(ArgsName argsName) throws Exception {
+        List<String> filterVal = new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        String delimiter = argsName.get("delimiter");
+        String out = argsName.get("out");
+        String path =  argsName.get("path");
+        String filter = argsName.get("filter");
+        var scanner = new Scanner(new BufferedReader(new FileReader(argsName.get(path))));
+        if (scanner.hasNext()) {
+            String[] arg = scanner.nextLine().split(delimiter);
+            StringJoiner tempLine = new StringJoiner(";");
+            for (int i = 0; i < arg.length; i++) {
+                if (filter.contains(arg[i])) {
+                    filterVal.add(String.valueOf(i));
+                } else if (filterVal.contains(arg[i])) {
+                    tempLine.add(String.valueOf(i));
+                }
+            }
+            list.add(tempLine.toString());
+        }
+        if ("stdout".equals(out)) {
+            list.forEach(System.out::println);
+        } else {
+            saveResult(list, out);
+        }
+    }
 
+    public static void saveResult(List<String> log, String file) throws Exception {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)))) {
+            log.forEach(out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        valid(args);
+        ArgsName argsName = ArgsName.of(args);
+        handle(argsName);
     }
 }
