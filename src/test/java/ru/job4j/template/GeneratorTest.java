38140e553a40f40c02ded8cb468525b4b5deddf1
Александр <palenko.as@mail.ru>
2022-02-07 14:23:06 +0300

    3. Шаблонизатор. [#855 #260871]

--- src/test/java/ru/job4j/template/GeneratorTest.java
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -2,25 +2,26 @@ package ru.job4j.template;
 
 import org.junit.Ignore;
 import org.junit.Test;
-import ru.job4j.map.Map;
 import ru.job4j.tdd.User;
 
 import java.util.HashMap;
+import java.util.Map;
 
-import static org.junit.Assert.assertThat;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class GeneratorTest {
-/*
+
     @Ignore
     @Test
     public void whenGenerate() {
-        User user =  new User();
+        User user = new User();
         Map<String, String> generate = new HashMap<>();
-        generate.put("Petr Arsentev", "you");
+        generate.put("name", "subject");
         String expected = "I am a Petr Arsentev, Who are you? ";
         String rsl = user.produce("I am a ${name}, Who are ${subject}? ", generate);
-        assertThat(rsl, expected);
+        assertEquals(rsl, expected);
     }
- */
+
 }
