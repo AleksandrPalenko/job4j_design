6beebcfa89e15863474e2f3c2db1e7c149c060c8
Александр <palenko.as@mail.ru>
2022-02-07 17:04:39 +0300

    3. Шаблонизатор. [#855 #260871]

--- src/test/java/ru/job4j/template/GeneratorTest.java
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 
 public class GeneratorTest {
@@ -19,9 +20,56 @@ public class GeneratorTest {
         User user = new User();
         Map<String, String> generate = new HashMap<>();
         generate.put("name", "subject");
+        generate.put("Petr Arsentev", "you");
         String expected = "I am a Petr Arsentev, Who are you? ";
         String rsl = user.produce("I am a ${name}, Who are ${subject}? ", generate);
         assertEquals(rsl, expected);
     }
 
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenGenerateIncorrectResultWithKey() {
+        User user = new User();
+        Map<String, String> generate = new HashMap<>();
+        generate.put("userName", "subject");
+        generate.put("Petr Arsentev", "you");
+        String expected = "I am a Petr Arsentev, Who are you? ";
+        String rsl = user.produce("I am a ${name}, Who are ${subject}? ", generate);
+        assertEquals(rsl, expected);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenGenerateIncorrectResultWithValue() {
+        User user = new User();
+        Map<String, String> generate = new HashMap<>();
+        generate.put("name", "anySubject");
+        generate.put("Petr Arsentev", "you");
+        String expected = "I am a Petr Arsentev, Who are you? ";
+        String rsl = user.produce("I am a ${name}, Who are ${subject}? ", generate);
+        assertEquals(rsl, expected);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenGenerateIncorrectWithAmountValue() {
+        User user = new User();
+        Map<String, String> generate = new HashMap<>();
+        generate.put("name", "subject");
+        generate.put("Petr Arsentev", "you");
+        generate.put("Petr", "you");
+        generate.put("Arsentev", "you");
+        String expected = "I am a Petr Arsentev, Who are you? ";
+        String rsl = user.produce("I am a ${name}, Who are ${subject}? ", generate);
+        assertEquals(rsl, expected);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenGenerateIncorrectWithNullValue() {
+        User user = new User();
+        Map<String, String> generate = new HashMap<>();
+        String rsl = user.produce("I am a ${name}, Who are ${subject}? ", generate);
+        assertNull(rsl);
+    }
 }
