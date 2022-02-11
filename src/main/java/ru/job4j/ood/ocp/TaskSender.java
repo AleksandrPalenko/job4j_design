4e8ec047c41340e8558b7a819e291ee59dc8a5a7
Александр <palenko.as@mail.ru>
2022-02-11 17:23:09 +0300

    0. Принцип открытости закрытости [#4914]

---
+++ src/main/java/ru/job4j/ood/ocp/TaskSender.java
@@ -0,0 +1,17 @@
+package ru.job4j.ood.ocp;
+
+public class TaskSender {
+    /*
+    public void send(String text, MemTask place ) {
+        if (place == MemTask.GIT) {
+            sendGIT(text);
+        } else if (place == MemTask.SITE) {
+            sendSITE(text);
+        }
+    }
+     Здесь идет нарушение принципа OCP, так как если нам нужно будет добавить новый функционал -
+     отправление задания через телеграмм, то нужно менять весь класс, а вместо этого, если сделать один интерфейс
+     с методом send(), чтобы к нему реализовать любые стратегии.
+     */
+
+}
