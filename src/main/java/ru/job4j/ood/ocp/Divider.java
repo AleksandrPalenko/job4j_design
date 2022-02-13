1618c07a3360c2286bba78d19c55f0b0ba4b13fa
Александр <palenko.as@mail.ru>
2022-02-13 17:38:10 +0300

    0. Принцип открытости закрытости [#4914 #263341]

---
+++ src/main/java/ru/job4j/ood/ocp/Divider.java
@@ -0,0 +1,13 @@
+package ru.job4j.ood.ocp;
+
+
+public class Divider {
+    public void isValid(int num) {
+
+    }
+
+    public static void main(String[] args) {
+        Divider divider = new Divider();
+        divider.isValid(10);
+    }
+}
