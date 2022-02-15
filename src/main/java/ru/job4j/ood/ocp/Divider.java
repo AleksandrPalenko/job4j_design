95342f222f4b211e0acb18aa426f24e7a1aafe52
Александр <palenko.as@mail.ru>
2022-02-15 13:56:07 +0300

    0. Принцип открытости закрытости [#4914 #263341]

--- src/main/java/ru/job4j/ood/ocp/Divider.java
+++ src/main/java/ru/job4j/ood/ocp/Divider.java
@@ -1,13 +1,21 @@
 package ru.job4j.ood.ocp;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Divider {
-    public void isValid(int num) {
-
+    public void isValid(ArrayList<Integer> num) {
+/*
+Здесь в параметрах нарушение OCP, нужно принимать абстракцию List<Integer> num
+ */
     }
 
-    public static void main(String[] args) {
-        Divider divider = new Divider();
-        divider.isValid(10);
-    }
+   public ArrayList<Integer> isNotValid(List<Integer> num) {
+        return null;
+        /*
+Здесь нарушение OCP в возращаемомо типе,
+нужно написать абстракцию,например List<I> isNotValid(List<Integer> num)
+ */
+   }
 }
