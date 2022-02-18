296de2d03a123f38e1a467551fc9f0ece7ff7339
Александр <palenko.as@mail.ru>
2022-02-18 19:02:57 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851 #263345]

--- src/test/java/ru/job4j/design/srp/ReportXmlTest.java
+++ src/test/java/ru/job4j/design/srp/ReportXmlTest.java
@@ -24,11 +24,35 @@ public class ReportXmlTest {
         StringBuilder expect = new StringBuilder()
                 .append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>")
                 .append("\n<employees>")
+                .append("    <employee>\n")
                 .append("\n<fired>").append(date).append("</fired>")
                 .append("\n<hired>").append(date).append("</hired>")
                 .append("\n<name>").append(worker.getName()).append("</fired>")
                 .append("\n<salary>").append(worker.getSalary()).append("</fired>")
                 .append("\n</employees>");
+        assertThat(engine.generate(em -> true), is(expect.toString()));
+    }
+
+    @Test
+    public void whenXMLGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        OffsetDateTime date = OffsetDateTime.ofInstant(now.toInstant(), now.getTimeZone().toZoneId());
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        ReportXml engine = new ReportXml(store);
+        StringBuilder expect = new StringBuilder()
+                .append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n")
+                .append("<employees>\n")
+                .append("    <employees ")
+                .append("name=\"").append(worker.getName()).append("\"")
+                .append(" hired=\"")
+                .append(date).append("\"")
+                .append(" fired=\"")
+                .append(date).append("\"")
+                .append(" salary=\"")
+                .append(worker.getSalary()).append("\"/>").append("\n")
+                .append("</employees>\n");
         assertThat(engine.generate(em -> true), is(expect.toString()));
     }
 }
