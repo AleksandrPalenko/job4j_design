7d7ee21ec93235fe5247fb86841ebc7e68a266a9
Александр <palenko.as@mail.ru>
2022-02-18 19:32:24 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851 #263345]

--- src/test/java/ru/job4j/design/srp/ReportXmlTest.java
+++ src/test/java/ru/job4j/design/srp/ReportXmlTest.java
@@ -12,27 +12,6 @@ import static org.junit.Assert.*;
 
 public class ReportXmlTest {
 
-    @Ignore
-    @Test
-    public void whenXMLGenerate() {
-        MemStore store = new MemStore();
-        Calendar date = new GregorianCalendar(2022, Calendar.FEBRUARY, 1);
-        Employee worker = new Employee("Ivan", date, date, 100);
-        date.setTimeZone(TimeZone.getTimeZone(ZoneOffset.of("+3")));
-        store.add(worker);
-        ReportXml engine = new ReportXml(store);
-        StringBuilder expect = new StringBuilder()
-                .append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>")
-                .append("\n<employees>")
-                .append("    <employee>\n")
-                .append("\n<fired>").append(date).append("</fired>")
-                .append("\n<hired>").append(date).append("</hired>")
-                .append("\n<name>").append(worker.getName()).append("</fired>")
-                .append("\n<salary>").append(worker.getSalary()).append("</fired>")
-                .append("\n</employees>");
-        assertThat(engine.generate(em -> true), is(expect.toString()));
-    }
-
     @Test
     public void whenXMLGenerated() {
         MemStore store = new MemStore();
