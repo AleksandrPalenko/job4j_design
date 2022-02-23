ddcf53f8815dc1a57cb5653be97fc7e79bb9731b
Александр <palenko.as@mail.ru>
2022-02-23 19:01:09 +0300

    1. Хранилище продуктов [#852 #264309]

--- src/main/java/ru/job4j/solid/lsp/Storage.java
+++ src/main/java/ru/job4j/solid/lsp/Storage.java
@@ -7,10 +7,6 @@ public interface Storage {
     boolean add(Food food);
 
     default int getPercent(Food food) {
-        //LocalDate date = LocalDate.now();
-        //Duration shelfLife = Duration.between(food.getCreateDate(), food.getExpiryDate());
-        //Duration dayBeforeExpired = Duration.between(date, food.getCreateDate());
-        //return (int) Math.abs((dayBeforeExpired.toDays() / shelfLife.toDays()) * 100);
         return (LocalDate.now().getDayOfYear() - food.getCreateDate().getDayOfYear()) * 100
                 / (food.getExpiryDate().getDayOfYear() - food.getCreateDate().getDayOfYear());
     }
